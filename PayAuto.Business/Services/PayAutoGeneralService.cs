11a25c46228b045b4d1b5086f920632f4e3b7b00
leonardoleal <leonardoleal@CON-L1BV02A>
2023-03-23 11:58:15 -0300

    .

--- PayAuto.Business/Services/PayAutoGeneralService.cs
+++ PayAuto.Business/Services/PayAutoGeneralService.cs
@@ -14,11 +14,8 @@ namespace PayAuto.Business.Services
         public PayAutoSPService spService = new PayAutoSPService();
         public void ApiCall(ref ComboBox comboUf)
         {
-            var stateClient = RestService.For<IStateApiService>("http://www.geonames.org");
-            var state = stateClient.GetStatesAsync();
-
-            
-
+            //var stateClient = RestService.For<IStateApiService>("http://www.geonames.org");
+           // var state = stateClient.GetStatesAsync();
         }
         public void SimpleProcess(string link ="www.uol.com.br")
         {

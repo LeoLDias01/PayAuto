462a21658734c7420ba48a25a234eea66fd6eb5d
leonardoleal <leonardoleal@CON-L1BV02A>
2023-03-31 11:32:56 -0300

    .

---
+++ PayAuto/Screens/frmMultipleSearch.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace PayAuto.Screens
+{
+    partial class frmMultipleSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMultipleSearch));
+            this.gpbAddress = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.btnSearchAddress = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnSimpleSearch = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.colRenavam = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPlaca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colChassi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colState = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gpbAddress.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gpbAddress
+            // 
+            this.gpbAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gpbAddress.Controls.Add(this.label6);
+            this.gpbAddress.Controls.Add(this.txtCep);
+            this.gpbAddress.Controls.Add(this.btnSearchAddress);
+            this.gpbAddress.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.gpbAddress.ForeColor = System.Drawing.Color.White;
+            this.gpbAddress.Location = new System.Drawing.Point(12, 12);
+            this.gpbAddress.Name = "gpbAddress";
+            this.gpbAddress.Size = new System.Drawing.Size(799, 79);
+            this.gpbAddress.TabIndex = 33;
+            this.gpbAddress.TabStop = false;
+            this.gpbAddress.Text = "Importação de Arquivo de Lote";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(6, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 19);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Selecione o arquivo";
+            // 
+            // txtCep
+            // 
+            this.txtCep.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCep.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCep.Location = new System.Drawing.Point(144, 39);
+            this.txtCep.MaxLength = 11;
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(607, 23);
+            this.txtCep.TabIndex = 1;
+            // 
+            // btnSearchAddress
+            // 
+            this.btnSearchAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearchAddress.BackColor = System.Drawing.Color.Transparent;
+            this.btnSearchAddress.FlatAppearance.BorderSize = 0;
+            this.btnSearchAddress.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearchAddress.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSearchAddress.ForeColor = System.Drawing.Color.White;
+            this.btnSearchAddress.Image = global::PayAuto.Properties.Resources.search_Dir__White_32x32;
+            this.btnSearchAddress.Location = new System.Drawing.Point(757, 30);
+            this.btnSearchAddress.Name = "btnSearchAddress";
+            this.btnSearchAddress.Size = new System.Drawing.Size(36, 34);
+            this.btnSearchAddress.TabIndex = 2;
+            this.btnSearchAddress.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.RoyalBlue;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colRenavam,
+            this.colPlaca,
+            this.colChassi,
+            this.colState});
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 97);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.RowHeadersVisible = false;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(799, 285);
+            this.dataGridView1.TabIndex = 34;
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHelp.BackColor = System.Drawing.Color.Transparent;
+            this.btnHelp.FlatAppearance.BorderSize = 2;
+            this.btnHelp.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
+            this.btnHelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHelp.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.btnHelp.ForeColor = System.Drawing.Color.White;
+            this.btnHelp.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnHelp.Location = new System.Drawing.Point(283, 396);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(259, 42);
+            this.btnHelp.TabIndex = 36;
+            this.btnHelp.Text = "Suporte";
+            this.btnHelp.UseVisualStyleBackColor = false;
+            // 
+            // btnSimpleSearch
+            // 
+            this.btnSimpleSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSimpleSearch.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnSimpleSearch.Enabled = false;
+            this.btnSimpleSearch.FlatAppearance.BorderSize = 2;
+            this.btnSimpleSearch.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.btnSimpleSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSimpleSearch.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSimpleSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSimpleSearch.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSimpleSearch.Location = new System.Drawing.Point(12, 396);
+            this.btnSimpleSearch.Name = "btnSimpleSearch";
+            this.btnSimpleSearch.Size = new System.Drawing.Size(259, 42);
+            this.btnSimpleSearch.TabIndex = 35;
+            this.btnSimpleSearch.Text = "Processar";
+            this.btnSimpleSearch.UseVisualStyleBackColor = false;
+            // 
+            // btnClean
+            // 
+            this.btnClean.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClean.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnClean.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClean.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.btnClean.ForeColor = System.Drawing.Color.White;
+            this.btnClean.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClean.Location = new System.Drawing.Point(552, 395);
+            this.btnClean.Margin = new System.Windows.Forms.Padding(7, 10, 3, 3);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(259, 43);
+            this.btnClean.TabIndex = 37;
+            this.btnClean.Text = "Limpar";
+            this.btnClean.UseVisualStyleBackColor = false;
+            // 
+            // colRenavam
+            // 
+            this.colRenavam.HeaderText = "Renavam";
+            this.colRenavam.Name = "colRenavam";
+            this.colRenavam.ReadOnly = true;
+            // 
+            // colPlaca
+            // 
+            this.colPlaca.HeaderText = "Placa";
+            this.colPlaca.Name = "colPlaca";
+            this.colPlaca.ReadOnly = true;
+            // 
+            // colChassi
+            // 
+            this.colChassi.HeaderText = "Chassi";
+            this.colChassi.Name = "colChassi";
+            this.colChassi.ReadOnly = true;
+            // 
+            // colState
+            // 
+            this.colState.HeaderText = "Estado";
+            this.colState.Name = "colState";
+            this.colState.ReadOnly = true;
+            // 
+            // frmMultipleSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(25)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(823, 450);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnSimpleSearch);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.gpbAddress);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMultipleSearch";
+            this.Text = "..:: Busca em lote";
+            this.gpbAddress.ResumeLayout(false);
+            this.gpbAddress.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpbAddress;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCep;
+        private System.Windows.Forms.Button btnSearchAddress;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnSimpleSearch;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colRenavam;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPlaca;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colChassi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colState;
+    }
+}
